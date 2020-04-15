ada45b8606fe19a83deaf2042efde405c86ce19a
junkeon <35945268+junkeon@users.noreply.github.com>
2020-04-15 23:40:41 +0900

    Update prob043.py

--- day016/prob043.py
+++ day016/prob043.py
@@ -1,18 +1,13 @@
-def solution(bridge_length, weight, truck_weights):
-    tic = 0
-    queue = []
-    total_weight = 0
-
-    while truck_weights or queue:
-        tic += 1
-
-        if queue and tic - queue[0][1] == bridge_length:
-            truck, _ = queue.pop(0)
-            total_weight -= truck      
-
-        if truck_weights and total_weight + truck_weights[0] <= weight:
-            truck = truck_weights.pop(0)
-            queue.append([truck, tic])
-            total_weight += truck
-
-    return tic
+def solution(prices):
+    answer = [0] * len(prices)
+    stack = []
+    for i in range(len(prices)-1):
+        while stack and stack[-1][1] > prices[i]:
+            j, _ = stack.pop()
+            answer[j] = i - j
+        stack.append((i, prices[i]))
+            
+    for i, _ in stack:
+        answer[i] = len(prices) - i - 1
+        
+    return answer
